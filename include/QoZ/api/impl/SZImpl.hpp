e2cabff9e8002d89d5e30003cf25f042cb322e51
Jliu-1 <jliu217@central.uh.edu>
2024-10-14 17:20:00 -0500

    bugfix

--- include/QoZ/api/impl/SZImpl.hpp
+++ include/QoZ/api/impl/SZImpl.hpp
@@ -8,16 +8,16 @@
 
 
 template<class T, QoZ::uint N>
-char *SZ_compress_impl(QoZ::Config &conf, const T *data, size_t &outSize) {
+char *SZ_compress_impl(QoZ::Config &conf, T *data, size_t &outSize) {
 #ifndef _OPENMP
     conf.openmp=false;
 #endif
     if (conf.openmp) {
         return SZ_compress_OMP<T, N>(conf, data, outSize);
     } else {
-        std::vector<T> dataCopy(data, data + conf.num);
+       
         //std::cout<<"implstart"<<std::endl;
-        auto output=SZ_compress_dispatcher<T, N>(conf, dataCopy.data(), outSize);
+        auto output=SZ_compress_dispatcher<T, N>(conf, data, outSize);
         //std::cout<<"implend"<<std::endl;
        
         return output;
