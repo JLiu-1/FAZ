2a6c0a9de24f3f27121115a642cb292349690991
Jliu-1 <jliu217@central.uh.edu>
2025-05-01 19:48:23 -0500

    upd

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -103,6 +103,7 @@ char *SPERR_Compress(QoZ::Config &conf, T *data, size_t &outSize){//only support
             
         char * outData=new char[stream.size()+conf.size_est()];
         outSize=stream.size();
+        std::cout<<outSize<<std::endl;
         memcpy(outData,stream.data(),stream.size());//maybe not efficient
         stream.clear();
         stream.shrink_to_fit();
