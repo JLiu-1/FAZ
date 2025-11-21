52be0383121f8419057f8619570dc2505a277b06
Jliu-1 <jliu217@central.uh.edu>
2025-11-21 16:36:52 -0600

    0.8.4

--- include/QoZ/sperr/SPECK2D_INT_ENC.h
+++ include/QoZ/sperr/SPECK2D_INT_ENC.h
@@ -61,6 +61,28 @@ void sperr::SPECK2D_INT_ENC<T>::m_process_S(size_t idx1,
 }
 
 template <typename T>
+void sperr::SPECK2D_INT_ENC<T>::m_process_S(size_t idx1,
+                                            size_t idx2,
+                                            size_t& counter,
+                                            bool need_decide)
+{
+  auto& set = m_LIS[idx1][idx2];
+  assert(!set.is_pixel());
+  bool is_sig = true;
+
+  if (need_decide) {
+    is_sig = m_decide_S_significance(set);
+    m_bit_buffer.wbit(is_sig);
+  }
+
+  if (is_sig) {
+    counter++;
+    m_code_S(idx1, idx2);
+    set.make_empty();
+  }
+}
+
+template <typename T>
 void sperr::SPECK2D_INT_ENC<T>::m_process_P(size_t idx, size_t& counter, bool need_decide)
 {
   bool is_sig = true;
@@ -99,11 +121,21 @@ auto sperr::SPECK2D_INT_ENC<T>::m_decide_S_significance(const Set2D& set) const 
 {
   assert(!set.is_empty());
 
-  const auto gtr = [thrd = m_threshold](auto v) { return v >= thrd; };
-  for (auto y = set.start_y; y < (set.start_y + set.length_y); y++) {
-    auto first = m_coeff_buf.cbegin() + y * m_dims[0] + set.start_x;
-    if (std::any_of(first, first + set.length_x, gtr))
-      return true;
+  // Only use SIMD implementation with 16 or more elements.
+  if (set.length_x < 16) {
+    for (auto y = set.start_y; y < (set.start_y + set.length_y); y++) {
+      auto first = m_coeff_buf.data() + y * m_dims[0] + set.start_x;
+      if (std::any_of(first, first + set.length_x,
+                      [thld = m_threshold](auto v) { return v >= thld; }))
+        return true;
+    }
+  }
+  else {
+    for (auto y = set.start_y; y < (set.start_y + set.length_y); y++) {
+      auto first = m_coeff_buf.data() + y * m_dims[0] + set.start_x;
+      if (sperr::any_ge(first, set.length_x, m_threshold))
+        return true;
+    }
   }
   return false;
 }
@@ -111,23 +143,32 @@ auto sperr::SPECK2D_INT_ENC<T>::m_decide_S_significance(const Set2D& set) const 
 template <typename T>
 auto sperr::SPECK2D_INT_ENC<T>::m_decide_I_significance() const -> bool
 {
-  const auto gtr = [thrd = m_threshold](auto v) { return v >= thrd; };
-
   // First, test the bottom rectangle.
   // It's stored in a contiguous chunk of memory till the buffer end.
   //
   assert(m_I.length_x == m_dims[0]);
-  auto first = m_coeff_buf.cbegin() + size_t{m_I.start_y} * size_t{m_I.length_x};
-  if (std::any_of(first, m_coeff_buf.cend(), gtr))
+  auto first = m_coeff_buf.data() + size_t{m_I.start_y} * size_t{m_I.length_x};
+  auto last = m_coeff_buf.data() + m_coeff_buf.size();
+  if (sperr::any_ge(first, last - first, m_threshold))
     return true;
 
   // Second, test the rectangle that's directly to the right of the missing top-left corner.
+  // Only use SIMD implementation with 16 or more elements.
   //
-  for (auto y = 0; y < m_I.start_y; y++) {
-    first = m_coeff_buf.cbegin() + y * m_dims[0] + m_I.start_x;
-    auto last = m_coeff_buf.cbegin() + (y + 1) * m_dims[0];
-    if (std::any_of(first, last, gtr))
-      return true;
+  auto len = m_dims[0] - m_I.start_x;
+  if (len < 16) {
+    for (auto y = 0; y < m_I.start_y; y++) {
+      first = m_coeff_buf.data() + y * m_dims[0] + m_I.start_x;
+      if (std::any_of(first, first + len, [thld = m_threshold](auto v) { return v >= thld; }))
+        return true;
+    }
+  }
+  else {
+    for (auto y = 0; y < m_I.start_y; y++) {
+      first = m_coeff_buf.data() + y * m_dims[0] + m_I.start_x;
+      if (sperr::any_ge(first, len, m_threshold))
+        return true;
+    }
   }
   return false;
 }
