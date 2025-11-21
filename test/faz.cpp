0184ac776812c21250c4d2e2223633fda2bbdc8a
Jliu-1 <jliu217@central.uh.edu>
2025-11-21 17:10:46 -0600

    0.8.4

--- test/faz.cpp
+++ test/faz.cpp
@@ -216,15 +216,6 @@ void decompress(char *inPath, char *cmpPath, char *decPath,
 int main(int argc, char *argv[]) {
 
 
-    py::scoped_interpreter guard{};
-
-    py::exec(R"(
-import sys
-print(">>> embedded Python info:")
-print("  executable:", sys.executable)
-print("  version   :", sys.version)
-print("  sys.path  :", sys.path)
-)");
 
 
 
